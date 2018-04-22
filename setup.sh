9a42993050d760577d5eba1f7a308208118bbedc
lihaoran02 <lihaoran02@baidu.com>
2018-04-22 22:16:50 +0800

    init

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# cleanup
+if [ -f ~/.vimrc ]; then
+    rm ~/.vimrc
+fi
+
+for var in $*
+do
+    if [ "$var" = "-r" ];  then
+        rm -rf ~/.vim
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        rm -rf ~/.vim/colors
+        mkdir ~/.vim/colors
+    fi
+done
+
+cp .vimrc ~/.vimrc
+cp ./vimrc.bundle ~/.vim/vimrc.bundle
+if [ -f "./vimrc.additional.bundle" ]; then
+    cp ./vimrc.additional.bundle ~/.vim/vimrc.additional.bundle
+fi
+
+vim -E -s <<-EOF
+:source ~/.vimrc
+:qa
+EOF
+vim +PlugInstall +qall
+
