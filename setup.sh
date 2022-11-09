1cdf093fb0c9ae87e07c6ff9d266260a7e7f2c53
ryanhrli <ryanhrli@tencent.com>
2022-11-09 11:02:43 +0800

    updat

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp vimrc ~/.vimrc
+cp vimrc.plug ~/.vimrc.plug
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
